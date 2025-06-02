55c2df2f78c5fcd6916daf01d8a933df4f4b439e
yogendrabaskota <yogendrabaskota18@gmail.com>
2025-06-02 23:16:11 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Navbar from './components/Navbar'
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+  
+    <BrowserRouter>
+    <Navbar />
+    <Routes>
+
+
+
+
+
+
+    </Routes>
+      
+    
+    
+    </BrowserRouter>
+    
+    </>
+
+
+      
+    
+  )
+}
+
+export default App
